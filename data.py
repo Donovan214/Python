87b1367802e1bb87102ba8a362b7ab8d7d2f044f
Aaron Donovan <adonovan214@students.mchenry.edu>
2024-08-26 14:39:59 -0500

    Add files via upload

---
+++ data.py
@@ -0,0 +1,20 @@
+# This is a Python program to demostrate different data types
+
+# Literal
+print ("literal;", 10)
+
+# Integer
+y = -10
+print("Integer:", y)
+
+# Float
+z = 12.5
+print("Float:", z)
+
+# String
+name = "Tim"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
