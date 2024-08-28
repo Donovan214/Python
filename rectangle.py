3a94d29b9ac5c0cbd1d740427f8e804ac0055ac6
Aaron Donovan <adonovan214@students.mchenry.edu>
2024-08-28 13:50:45 -0500

    Add files via upload

--- rectangle.py
+++ rectangle.py
@@ -2,5 +2,6 @@ length = 15
 width = 20
 area = 300
 
-print("the length of", length, "and a width of", width, "is", area)
+print(length * width)
 
+print("the length of", length, "and a width of", width, "is", area)
