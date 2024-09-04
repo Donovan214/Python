ceb0be9343b5004c4e951f4824b5c38136a923f0
Aaron Donovan <adonovan214@students.mchenry.edu>
2024-09-04 13:40:07 -0500

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,10 @@
+"""
+Type the above code into Visual Studio Code
+Add a minimum of 4 comments to the above Python 
+program to make it more understandable.
+    """
+tank_size = 15
+distance = 600  # how far before refill
+mpg = distance / tank_size  # The amount of gas used per tank
+
+print("Miles per gallon:", mpg)  # final result
