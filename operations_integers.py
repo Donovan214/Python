502ef755370bfef839f202967e5a08ee65fcb29a
Aaron Donovan <adonovan214@students.mchenry.edu>
2024-09-22 13:59:43 -0500

    Add files via upload

--- operations_integers.py
+++ operations_integers.py
@@ -2,14 +2,22 @@
 
 # gathering information
 
-integer_1 = int(input("Enter an integer  "))
-integer_2 = int(input("Enter another integer  "))
+integer_1 = int(input("Enter an integer A  "))
+integer_2 = int(input("Enter another integer B  "))
 # and operator
-if integer_1 > integer_2:
+if integer_1 > 0 and integer_2 > 0:
     print("Both numbers are greater than 0")
-elif integer_2 < integer_1:
+if integer_1 < 100 and integer_2 < 100:
     print("Both numbers are lower than 100")
-else:
-    print("Neither number is greater than 0")
 
 # or operator
+if integer_1 > 0 or integer_2 > 0:
+    print("One of the numbers is greater than 0")
+else:
+    print("Neither number is great than 0")
+
+# not operator
+if not integer_1 > integer_2:
+    print("A in not bigger than B.")
+else:
+    print("A is greater than B")
