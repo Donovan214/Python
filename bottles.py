5536a86a07309248b6abf7320910b90e27fb1773
Aaron Donovan <adonovan214@students.mchenry.edu>
2024-09-23 14:03:00 -0500

    Update bottles.py

--- bottles.py
+++ bottles.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """Demostrate the while loop using the 99 bottles"""
 
 # counting down from 99
@@ -18,24 +17,3 @@ print("1 bottle of beer on the wall")
 print("1 bottle of beer")
 print("take one down, pass it around")
 print("No bottles of beer on the wall")
-=======
-"""Demostrate the while loop using the 99 bottles"""
-
-# counting down from 99
-count = 99
-while count > 2:  # stopped at 2 in order to write last part
-    # displaing the count of the starting point
-    print(count, "bottles of beer on the wall")
-    print(count, "bottles of beer")
-    print("Take one down, pass it around")
-    count -= 1  # this takes a bottle away
-    print(count, "bottles of beer on the wall!")
-print("2 bottles of beer on the wall")  # had to write last bottle
-print("2 bottles of beer")
-print("take one down, pass it around")
-print("1 bottle of beer on the wall")  # changed to correct grammer
-print("1 bottle of beer on the wall")
-print("1 bottle of beer")
-print("take one down, pass it around")
-print("No bottles of beer on the wall")
->>>>>>> 502ef755370bfef839f202967e5a08ee65fcb29a
